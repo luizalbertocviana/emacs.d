afc21119bfb11da655262e6c7d571bc57b44989e
Luiz Alberto <luizalbertocviana@gmail.com>
2020-07-12 19:20:22 -0300

    overriding dired-dwim-target-directory to select targets globally

--- init.el
+++ init.el
@@ -345,6 +345,20 @@
       (interactive)
       (let* ((file (dired-get-filename nil t)))
         (call-process "xdg-open" nil 0 nil file)))
+    (defun dired-dwim-target-directory ()
+      (let* ((get-buffer-dir (lambda (buffer)
+                               (with-current-buffer buffer
+                                 (and (eq major-mode 'dired-mode)
+                                      (dired-current-directory)))))
+             (this-dir (funcall get-buffer-dir (current-buffer)))
+             (other-dir (some (lambda (buffer)
+                                (let ((buffer-dir (funcall get-buffer-dir buffer)))
+                                  (and (not (equal buffer-dir this-dir))
+                                       buffer-dir)))
+                              (buffer-list))))
+        (if dired-dwim-target
+            (or other-dir this-dir)
+          this-dir)))
   :hook
     (dired-mode . dired-omit-mode)
   :custom
