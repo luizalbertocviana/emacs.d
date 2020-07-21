ea7c8cf2ee6d3693591cd8f8bb640d60ec92dc48
Luiz Alberto <luizalbertocviana@gmail.com>
2020-07-21 17:29:53 -0300

    added company-fish setup

--- init.el
+++ init.el
@@ -419,6 +419,18 @@
       :custom
         (company-reftex-max-annotation-length 80)
     )
+    ;; fish-like completion for eshell
+    (use-package company-fish
+      :straight (company-fish :type   git
+                              :flavor melpa
+                              :host   github
+                              :repo   "CeleritasCelery/company-fish")
+      :config
+        (when (executable-find "fish")
+          (add-to-list 'company-backends 'company-fish)
+          (add-hook 'shell-mode-hook 'company-mode)
+          (add-hook 'eshell-mode-hook 'company-mode))
+    )
 )
 
 ;; org
