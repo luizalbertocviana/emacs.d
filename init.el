bfca85ff78bd7a882268a1cb61230b37b9d9ec72
Luiz Alberto <luizalbertocviana@gmail.com>
2020-08-11 11:22:41 -0300

    key-chord setup

--- init.el
+++ init.el
@@ -617,6 +617,25 @@
 ;; simple and effective interface to google translation service
 (use-package google-translate)
 
+;; key chords
+(use-package key-chord
+  :custom
+    (key-chord-two-keys-delay 0.3)
+    (key-chord-one-key-delay 0.3)
+  :config
+    (key-chord-mode 1)
+    ;; jk and kj return to normal state
+    (key-chord-define evil-visual-state-map "kj" 'evil-normal-state)
+    (key-chord-define evil-visual-state-map "jk" 'evil-normal-state)
+    (key-chord-define evil-insert-state-map "kj" 'evil-normal-state)
+    (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
+    ;; chords to avoid reaching to shift key
+    (key-chord-define evil-insert-state-map "88" "*")
+    (key-chord-define evil-insert-state-map "99" "(")
+    (key-chord-define evil-insert-state-map "--" "_")
+    (key-chord-define evil-insert-state-map "==" "+")
+    (key-chord-define evil-insert-state-map "[[" "{"))
+
 ;; keybindings
 (use-package general
   :preface
