9800a86a673f66cf57216cd983c82ca6707bc22c
Luiz Alberto <luizalbertocviana@gmail.com>
2020-10-09 21:04:51 -0300

    added keybindings for haskell mode

--- init.el
+++ init.el
@@ -1352,6 +1352,20 @@ only one in the current frame, kill the frame instead"
         "f" 'rust-format-buffer
         "r" 'recompile
     )
+    ;; haskell mode
+    (general-define-key
+      :states  '(normal)
+      :keymaps '(haskell-mode-map)
+      :prefix  "SPC m"
+        "C" 'haskell-cabal-visit-file
+        "I" 'haskell-mode-format-imports
+        "b" 'haskell-process-cabal-build
+        "c" 'haskell-process-cabal
+        "i" 'haskell-process-do-info
+        "l" 'haskell-process-load-file
+        "r" 'haskell-interactive-switch
+        "t" 'haskell-process-do-type
+    )
     ;; gnus
     (general-define-key
       :states  '(normal)
