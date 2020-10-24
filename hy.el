213ecf1de2fb6225dc5fcce1547e0153797d5cb2
Luiz Alberto <luizalbertocviana@gmail.com>
2020-10-24 18:43:48 -0300

    first version of hy.el

---
+++ hy.el
@@ -0,0 +1,6 @@
+;; hy mode
+(use-package hy-mode
+  :straight (hy-mode :type   git
+                     :flavor melpa
+                     :host   github
+                     :repo   "luizalbertocviana/hy-mode"))
