163ad38befaca074fdd415652e515602fd4664ff
Luiz Alberto <luizalbertocviana@gmail.com>
2020-10-24 22:18:21 -0300

    added keybindings

--- haskell-setup.el
+++ haskell-setup.el
@@ -1,3 +1,17 @@
 (use-package haskell-mode
   :hook
     (haskell-mode . interactive-haskell-mode))
+
+(general-define-key
+ :states  '(normal)
+ :keymaps '(haskell-mode-map)
+ :prefix  "SPC m"
+ "C" 'haskell-cabal-visit-file
+ "I" 'haskell-mode-format-imports
+ "b" 'haskell-process-cabal-build
+ "c" 'haskell-process-cabal
+ "i" 'haskell-process-do-info
+ "l" 'haskell-process-load-file
+ "r" 'haskell-interactive-switch
+ "t" 'haskell-process-do-type
+ )
