a7adb2acb9304dd8595fddee1a0f60872e7a1413
Luiz Alberto <luizalbertocviana@gmail.com>
2020-10-24 22:21:04 -0300

    some keybindings for c++ related modes

---
+++ cpp-setup.el
@@ -0,0 +1,21 @@
+(general-define-key
+ :states  '(normal)
+ :keymaps '(c++-mode-map)
+ :prefix  "SPC m"
+ "b" 'compile
+ "d" 'gdb
+ "e" 'next-error
+ "r" 'recompile
+ )
+;; gdb mode
+(general-define-key
+ :states  '(normal)
+ :keymaps '(gud-mode-map)
+ :prefix "SPC m"
+ "b" 'gdb-frame-breakpoints-buffer
+ "d" 'gdb-frame-disassembly-buffer
+ "i" 'gdb-frame-io-buffer
+ "l" 'gdb-frame-locals-buffer
+ "m" 'gdb-frame-memory-buffer
+ "s" 'gdb-frame-stack-buffer
+ )
