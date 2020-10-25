7fbaed9f51d05505dfbf69a4285c17006fb25108
Luiz Alberto <luizalbertocviana@gmail.com>
2020-10-24 22:20:24 -0300

    added keybindings

--- rust-setup.el
+++ rust-setup.el
@@ -1,3 +1,21 @@
 (use-package rustic
   :custom
     (rustic-lsp-client nil))
+
+(general-define-key
+ :states  '(normal)
+ :keymaps '(rustic-mode-map)
+ :prefix  "SPC m"
+ "C" 'rustic-cargo-clippy
+ "D a" 'rustic-cargo-add
+ "D r" 'rustic-cargo-rm
+ "D u" 'rustic-cargo-upgrade
+ "b" 'rustic-cargo-run
+ "c" 'rustic-cargo-check
+ "d" 'xref-find-definitions
+ "e" 'next-error
+ "f" 'rustic-format-buffer
+ "m" 'rustic-popup
+ "r" 'rustic-recompile
+ "t" 'rustic-cargo-test
+ )
