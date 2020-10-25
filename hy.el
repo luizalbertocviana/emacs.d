75faeaf3236de3eaba3044f862b0a0ec140beb6e
Luiz Alberto <luizalbertocviana@gmail.com>
2020-10-24 22:18:31 -0300

    added keybindings

--- hy.el
+++ hy.el
@@ -4,3 +4,14 @@
                      :flavor melpa
                      :host   github
                      :repo   "luizalbertocviana/hy-mode"))
+
+(general-define-key
+ :states  '(normal)
+ :keymaps '(hy-mode-map)
+ :prefix  "SPC m"
+ "b" 'hy-shell-eval-buffer
+ "d" 'hy-describe-thing-at-point
+ "e" 'hy-shell-eval-current-form
+ "i" 'hy-jedhy-update-imports
+ "r" 'run-hy
+ )
