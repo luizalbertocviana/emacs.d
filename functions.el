ebfe1e8d274a268b01e3da0742239b54b560a5a8
Luiz Alberto <luizalbertocviana@gmail.com>
2020-10-24 22:17:18 -0300

    added some functions

--- functions.el
+++ functions.el
@@ -9,17 +9,26 @@
 (defun dired-current-dir ()
   (dired "./"))
 
+(defun kill-buffer-and-frame-or-window ()
+  "kills current buffer and its window. When its window is the
+only one in the current frame, kill the frame instead"
+  (interactive)
+  (when (kill-buffer)
+    (if (one-window-p)
+        (delete-frame)
+      (delete-window))))
+
 (defun dired-dwim-target-directory ()
-    (let* ((get-buffer-dir (lambda (buffer)
-                             (with-current-buffer buffer
-                               (and (eq major-mode 'dired-mode)
-                                    (dired-current-directory)))))
-           (this-dir (funcall get-buffer-dir (current-buffer)))
-           (other-dir (some (lambda (buffer)
-                              (let ((buffer-dir (funcall get-buffer-dir buffer)))
-                                (and (not (equal buffer-dir this-dir))
-                                     buffer-dir)))
-                            (buffer-list))))
-      (if dired-dwim-target
-          (or other-dir this-dir)
-        this-dir)))
+  (let* ((get-buffer-dir (lambda (buffer)
+                           (with-current-buffer buffer
+                             (and (eq major-mode 'dired-mode)
+                                  (dired-current-directory)))))
+         (this-dir (funcall get-buffer-dir (current-buffer)))
+         (other-dir (seq-some (lambda (buffer)
+                                (let ((buffer-dir (funcall get-buffer-dir buffer)))
+                                  (and (not (equal buffer-dir this-dir))
+                                       buffer-dir)))
+                              (buffer-list))))
+    (if dired-dwim-target
+        (or other-dir this-dir)
+      this-dir)))
