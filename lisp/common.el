5fa7acf138e844561f18d1ddbb1fd643f47a4a6f
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2021-05-07 00:50:20 -0300

    removed company-fish setup

--- lisp/common.el
+++ lisp/common.el
@@ -85,17 +85,6 @@
     (use-package company-quickhelp
       :config
         (company-quickhelp-mode))
-    ;; fish-like completion for eshell
-    (use-package company-fish
-      :straight (company-fish :type   git
-                              :flavor melpa
-                              :host   github
-                              :repo   "CeleritasCelery/company-fish")
-      :config
-        (when (executable-find "fish")
-          (add-to-list 'company-backends 'company-fish)
-          (add-hook 'shell-mode-hook 'company-mode)
-          (add-hook 'eshell-mode-hook 'company-mode)))
     ;; yasnippet
     (use-package yasnippet
       :config
