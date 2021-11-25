86987331a7c7702581bc59b1319290b957a04683
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2021-11-25 16:21:29 -0300

    adds hooks and keybindings to typescript setup

--- lisp/lang/typescript.el
+++ lisp/lang/typescript.el
@@ -6,3 +6,24 @@
          (before-save . tide-format-before-save)))
 
 (add-hook 'typescript-mode-hook 'lsp)
+(add-hook 'typescript-mode-hook (lambda ()
+                                  (tide-mode)
+                                  (tide-restart-server)))
+
+(general-define-key
+ :states '(normal)
+ :keymaps '(typescript-mode-map)
+ :prefix "SPC m"
+ "E"   'tide-project-errors
+ "F"   'tide-format
+ "R"   'tide-references
+ "d"   'tide-documentation-at-point
+ "e"   'tide-error-at-point
+ "f"   'tide-fix
+ "i"   'tide-organize-imports
+ "j"   'tide-jsdoc-template
+ "r f" 'tide-rename-file
+ "r s" 'tide-rename-symbol
+ "s l" 'tide-list-servers
+ "s r" 'tide-restart-server
+ "s v" 'tide-verify-setup)
