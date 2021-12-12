8332cdd8171b624998f1c09b942671f2b3702280
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2021-12-12 18:21:11 -0300

    enhances helm setup with projectile, ripgrep and wgrep stuff

--- lisp/helm-setup.el
+++ lisp/helm-setup.el
@@ -6,6 +6,30 @@
 
 (use-package helm-lsp)
 
+(use-package helm-projectile)
+
+(setq helm-grep-ag-command (concat "rg"
+                                   " --color=never"
+                                   " --smart-case"
+                                   " --no-heading"
+                                   " --line-number %s %s %s"))
+
+(setq helm-grep-file-path-style 'relative)
+
+(general-define-key
+  :states  '(normal)
+  :keymaps 'helm-grep-mode-map
+  :prefix "SPC m"
+    "a" 'wgrep-abort-changes
+    "c" 'wgrep-change-to-wgrep-mode
+    "d" 'wgrep-mark-deletion
+    "f" 'wgrep-finish-edit
+    "q" 'wgrep-exit
+    "s" 'wgrep-save-all-buffers
+    "u" 'wgrep-remove-change
+    "U" 'wgrep-remove-all-change
+)
+
 (define-key global-map [remap execute-extended-command] 'helm-M-x)
 
 (define-key helm-map (kbd "C-a") 'helm-execute-persistent-action)
