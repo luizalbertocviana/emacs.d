797dae46d259629179a05084944d7d67d891e1c3
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2022-08-11 17:44:54 -0300

    rearranges cape capf functions

--- lisp/common.el
+++ lisp/common.el
@@ -118,18 +118,18 @@
 
 (use-package cape
   :init
-    (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-    (add-to-list 'completion-at-point-functions #'cape-file)
-    (add-to-list 'completion-at-point-functions #'cape-history)
-    (add-to-list 'completion-at-point-functions #'cape-keyword)
-    (add-to-list 'completion-at-point-functions #'cape-tex)
-    (add-to-list 'completion-at-point-functions #'cape-sgml)
-    (add-to-list 'completion-at-point-functions #'cape-rfc1345)
-    (add-to-list 'completion-at-point-functions #'cape-abbrev)
-    (add-to-list 'completion-at-point-functions #'cape-ispell)
-    (add-to-list 'completion-at-point-functions #'cape-dict)
-    (add-to-list 'completion-at-point-functions #'cape-symbol)
-    (add-to-list 'completion-at-point-functions #'cape-line))
+    (add-to-list 'completion-at-point-functions #'cape-dabbrev t)
+    (add-to-list 'completion-at-point-functions #'cape-file t)
+    (add-to-list 'completion-at-point-functions #'cape-keyword t)
+    (add-to-list 'completion-at-point-functions #'cape-line t)
+    (add-to-list 'completion-at-point-functions #'cape-ispell t)
+    (add-to-list 'completion-at-point-functions #'cape-dict t)
+    (add-to-list 'completion-at-point-functions #'cape-history t)
+    (add-to-list 'completion-at-point-functions #'cape-symbol t)
+    (add-to-list 'completion-at-point-functions #'cape-tex t)
+    (add-to-list 'completion-at-point-functions #'cape-sgml t)
+    (add-to-list 'completion-at-point-functions #'cape-rfc1345 t)
+    )
 
 (use-package marginalia
   :init
