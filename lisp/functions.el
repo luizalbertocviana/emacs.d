b19fc60ce83f134a03d57ca839c4d05afde1f45c
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2022-08-27 15:24:27 -0300

    changes `line-ref-to-kill-ring` to return relative paths when inside projects

--- lisp/functions.el
+++ lisp/functions.el
@@ -24,6 +24,13 @@ only one in the current frame, kill the frame instead"
   "Copy to the kill ring a string in the format \"file-name:line-number\"
 for the current buffer's file name, and the line number at point."
   (interactive)
-  (kill-new
-   (format "%s:%d" (buffer-file-name) (save-restriction
-                                        (widen) (line-number-at-pos)))))
+  (let* ((absolute-line-ref (format "%s:%d"
+                                    (buffer-file-name)
+                                    (save-restriction
+                                      (widen) (line-number-at-pos))))
+         (project-root (projectile-project-root))
+         (line-ref (if project-root
+                       (string-remove-prefix project-root absolute-line-ref)
+                     absolute-line-ref)))
+    (kill-new line-ref)
+    (message line-ref)))
