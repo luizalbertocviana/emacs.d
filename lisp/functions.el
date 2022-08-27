caa6082f8a1bfc200c2d4d95543a46dbe5ac6b41
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2022-08-27 00:25:52 -0300

    adds `SPC f l` keybinding to copy "file:line" reference for current line

--- lisp/functions.el
+++ lisp/functions.el
@@ -19,3 +19,11 @@ only one in the current frame, kill the frame instead"
 (defun clear-repl-hook ()
   (evil-local-set-key 'normal (kbd "C-c C-l") 'clear-repl)
   (evil-local-set-key 'insert (kbd "C-c C-l") 'clear-repl))
+
+(defun position-to-kill-ring ()
+  "Copy to the kill ring a string in the format \"file-name:line-number\"
+for the current buffer's file name, and the line number at point."
+  (interactive)
+  (kill-new
+   (format "%s:%d" (buffer-file-name) (save-restriction
+                                        (widen) (line-number-at-pos)))))
--- lisp/keybindings.el
+++ lisp/keybindings.el
@@ -185,6 +185,7 @@
     "d"   '(:ignore t :which-key "dired")
     "d"   (lambda () (interactive) (dired "./"))
     "f"   'remapme-find
+    "l"   'position-to-kill-ring
     "m"   'list-bookmarks
     "o"   'find-file
     "p R" 'projectile-replace-regexp
