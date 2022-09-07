36436618a4a86daf23ee6b6c11ae81785ca750a9
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2022-09-07 00:19:47 -0300

    adds tree-sitter setup

--- lisp/common.el
+++ lisp/common.el
@@ -128,3 +128,14 @@
     (marginalia-mode))
 
 (use-package embark)
+
+(use-package tree-sitter
+  :config
+    ;; activate tree-sitter on any buffer containing code for which it has a parser available
+    (global-tree-sitter-mode)
+    ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
+    ;; by switching on and off
+    (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :after tree-sitter)
